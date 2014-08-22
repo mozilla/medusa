4af05fde8cd49e0c642ab2e782b7eac58295f815
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-08-23 00:07:34 +0100

    Refactoring.

--- src/clj/medusa/core.clj
+++ src/clj/medusa/core.clj
@@ -15,7 +15,7 @@
     (resource/metrics-resource params))
   (GET "/detectors/:detector_id/metrics/:metric_id" [& params]
     (resource/metrics-resource params))
-  (GET "/detectors/:detector_id/metrics/:metric_id/alerts" [& params]
+  (GET "/detectors/:detector_id/metrics/:metric_id/alerts/" [& params]
     (resource/alerts-resource params))
   (GET "/detectors/:detector_id/metrics/:metric_id/alerts/:id" [& params]
     (resource/alerts-resource params))
--- src/cljs/medusa/core.cljs
+++ src/cljs/medusa/core.cljs
@@ -8,53 +8,17 @@
 
 (enable-console-print!)
 
-(def event-channel (chan))
-
 (def app-state (atom {:detectors []
                       :metrics []
                       :alerts []
-                      :selected-detector []
+                      :selected-detector nil
+                      :selected-metric nil
                       :error {}}))
-
-(defn get-detectors-list [{:keys [detectors error]}]
-  (go
-    (GET "/detectors/"
-      {:handler
-       (fn [data]
-         (om/update! detectors data))
-
-       :error-handler
-       (fn [e]
-         (om/update! error [:message] (str "Failure to load detectors list, reason: " (:status-text e)))) })))
-
-(defn get-metrics-list [detector state]
-  (go
-    (GET (str "/detectors/" (:id @detector) "/metrics/")
-      {:handler
-       (fn [data]
-         (om/update! state [:metrics] data))
-
-       :error-handler
-       (fn [e]
-         (om/update! state [:error :message] (str "Failure to load metrics list, reason: " (:status-text e)))
-         (om/update! state [:metrics] []))})))
-
-(defn get-alerts-list [detector state]
-  (go
-    (GET (str "/detectors/" (:id @detector) "/alerts/")
-      {:handler
-       (fn [data]
-         (om/update! state [:alerts] data))
-
-       :error-handler
-       (fn [e]
-         (om/update! state [:error :message] (str "Failure to load metrics list, reason: " (:status-text e)))
-         (om/update! state [:alerts] []))})))
 
 (defn detector-list [{:keys [detectors selected-detector]}]
   (reify
-    om/IRender
-    (render [_]
+    om/IRenderState
+    (render-state [_ {:keys [event-channel]}]
       (html [:div.list-group
              (for [detector detectors]
                [:a.list-group-item
@@ -64,11 +28,14 @@
 
 (defn metrics-list [{:keys [metrics]}]
   (reify
-    om/IRender
-    (render [_]
-      (html [:select
-             (for [metric metrics]
-               [:option (:name metric)])]))))
+    om/IRenderState
+    (render-state [_ {:keys [event-channel]}]
+      (let [name>metric (zipmap (map :name metrics) metrics)]
+       (html [:select {:on-change #(let [selected-metric (get name>metric (.. % -target -value))]
+                                     (go
+                                       (>! event-channel [:metric-selected selected-metric])))}
+              (for [metric metrics]
+                [:option (:name metric)])])))))
 
 (defn alerts-list [{:keys [alerts]}]
   (reify
@@ -85,32 +52,63 @@
     (render [_]
       (html [:div (:message error)]))))
 
-(defn event-loop [state]
-  (go
-    (loop []
-      (let [[topic message] (<! event-channel)]
-        (condp = topic
-          :detector-selected
-          (do
-            (om/update! state [:error :message] "")
-            (om/update! state [:selected-detector] message)
-            (get-metrics-list message state)
-            (get-alerts-list message state)))
-        (recur)))))
+(defn get-resource [uri]
+  (let [ch (chan)]
+    (GET uri
+      {:handler #(go (>! ch {:data %}))
+       :error-handler #(go (>! ch {:error (str "Failure to load detectors list, reason: " (:status-text %))}))})
+    ch))
 
-(defn layout [state]
+(defn update-resource [state update-key uri]
+  (go
+    (let [{:keys [data error]} (<! (get-resource uri))]
+      (if-not error
+        (om/update! state [update-key] data)
+        (do
+          (om/update! state [update-key] nil)
+          (om/update! state [:error :message] error))))))
+
+(defn layout [state owner]
   (reify
+    om/IInitState
+    (init-state [_]
+      {:event-channel (chan)})
+
     om/IWillMount
     (will-mount [_]
-      (event-loop state)
-      (let [{:keys [detectors error]} state]
-       (get-detectors-list {:detectors detectors :error error})))
+      (update-resource state :detectors "/detectors/")
 
-    om/IRender
-    (render [_]
+      (go
+        (loop []
+          (let [event-channel (om/get-state owner :event-channel)
+                [topic message] (<! event-channel)]
+            (condp = topic
+              :detector-selected
+              (do
+                (om/update! state [:error :message] "")
+                (om/update! state [:selected-detector] message)
+                (update-resource state :metrics (str "/detectors/" (:id @message) "/metrics/"))
+                (update-resource state :alerts (str "/detectors/" (:id @message) "/alerts/")))
+
+              :metric-selected
+              (let [detector (:selected-detector @state)]
+                (om/update! state [:error :message] "")
+                (om/update! state [:selected-metric] message)
+                (update-resource
+                  state
+                  :alerts
+                  (str "/detectors/" (:id @detector) "/metrics/" (:id @message) "/alerts/"))))
+            (recur)))))
+
+    om/IRenderState
+    (render-state [_ {:keys [event-channel]}]
       (html [:div.container-fluid
-             (om/build detector-list (select-keys state [:detectors :selected-detector]))
-             (om/build metrics-list (select-keys state [:metrics]))
+             (om/build detector-list
+                       (select-keys state [:detectors :selected-detector])
+                       {:init-state {:event-channel event-channel}})
+             (om/build metrics-list
+                       (select-keys state [:metrics])
+                       {:init-state {:event-channel event-channel}})
              (om/build alerts-list (select-keys state [:alerts]))
              (om/build error-notification (select-keys state [:error]))]))))
 
