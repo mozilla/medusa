7d3fa4d17c239054f05681a89f8fb6670d8c2ff6
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-09-23 16:34:28 +0000

    Fix typo.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main "clj.medusa.core"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.16"]
                  [compojure "1.1.8"]
