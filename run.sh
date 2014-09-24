798e35d0c14db5048fb8ec65dca993a666b84571
Mark Reid <mreid@mozilla.com>
2014-09-24 11:54:07 -0300

    Fix paths for the `lein` executable.

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install openjdk-7-jdk
 echo "Setting up leiningen"
 if [ ! -f /usr/local/bin/lein ];
 then
-	sudo wget -P /usr/local/lein https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
-	sudo chmod +x /usr/local/lein
+	sudo wget -P /usr/local/bin https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+	sudo chmod +x /usr/local/bin/lein
 fi
 export PATH=$PWD:$PATH
 
