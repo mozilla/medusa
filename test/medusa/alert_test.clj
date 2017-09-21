d81c374019cc1fa92c0492921956c516976aca26
Erik Rose <erik@mozilla.com>
2017-09-21 16:26:33 -0400

    Combine the 2 bounding-buildids tests, since `testing` makes both assertions run, even if the first fails.
    Rename things to "test-whatever", because Clojure gets grumpy when I shadow things I've imported. This is the spelling convention they use in Leiningen itself. I find it redundant, but I don't have a better idea atm.

--- test/medusa/alert_test.clj
+++ test/medusa/alert_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [clj.medusa.alert]))
 
-(deftest build-range
+(deftest test-build-range
   (testing "single build in a day"
     (is (= (#'clj.medusa.alert/build-range "20170102030405" "20170102030405") "20170102030405")))
   (testing "multiple builds in a day"
--- test/medusa/changesets_test.clj
+++ test/medusa/changesets_test.clj
@@ -2,13 +2,11 @@
   (:require [clojure.test :refer :all]
             [clj.medusa.changesets :refer [bounding-buildids]]))
 
-(deftest bounding-buildids-single
-  (testing "bounding-buildids when there is only one buildid for the day"
-    (is (= (bounding-buildids "2017-08-02" "mozilla-central") ["20170802100302" "20170802100302"]))))
-
-(deftest bounding-buildids-multiple
-  (testing "bounding-buildids when there are multiple buildids for the day"
+(deftest test-bounding-buildids
+  (testing "when there is only one buildid for the day"
+    (is (= (bounding-buildids "2017-08-02" "mozilla-central") ["20170802100302" "20170802100302"])))
+  (testing "when there are multiple buildids for the day"
     (is (= (bounding-buildids "2017-09-18" "mozilla-central") ["20170918100059" "20170918220054"]))))
 
-(deftest buildid-from-dir
+(deftest test-buildid-from-dir
   (is (= (#'clj.medusa.changesets/buildid-from-dir "2017-09-21-10-01-41-mozilla-central/") "20170921100141")))
