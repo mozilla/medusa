d95738228373112c2de99a1bb14338e83cfaafaa
Chris H-C <chutten@mozilla.com>
2018-06-19 15:40:08 -0400

    Fixes #25 - Provide instructions in Alert emails

--- src/clj/medusa/alert.clj
+++ src/clj/medusa/alert.clj
@@ -39,12 +39,42 @@
       (let [[earliest-build latest-build] (changesets/bounding-buildids date "mozilla-central") ; get the buildids for the given date
             changeset-url (changesets/pushlog-url earliest-build latest-build "mozilla-central")]
         (log/info "Changeset URL" changeset-url)
-        (send-email (str "Alert for " metric_name " (" detector_name ") on " date)
-                    (str "Alert details: " alert-url
+        (send-email (str "Telemetry Alert for " metric_name " on " date)
+                    (str "We have detected a change in the Telemetry probe " metric_name " in Firefox Nightly builds from " date "."
                          "\n\n"
-                         "Evolution dashboard: " evo-url
+                         "Alert details: " alert-url
+                         "\n"
+                         "Changes new to Nightly builds on " date ": " changeset-url
+                         "\n"
+                         "The value of " metric_name " over time: " evo-url
                          "\n\n"
-                         "Changeset for " (build-range earliest-build latest-build) ": " changeset-url)
+                         "What to do about this:"
+                         "\n\n"
+                         "1. File a bug to track your investigation. You can just copy this email into the bug Description to get you started."
+                         "\n"
+                         "2. Reply-All to this email to let the list know that you are investigating. Include the bug number so we can help out."
+                         "\n"
+                         "3. Triage the alert. You can find instructions here: " alert-url
+                         "\n\n"
+                         "If you have any problems, please ask for help on the #telemetry IRC channel or on Slack in #fx-metrics. We'll give you a hand."
+                         "\n\n"
+                         "What this is:"
+                         "We have a system called cerberus[1] that compares Telemetry collected on different Nightly builds and "
+                         "looks for sudden changed in value distributions using the Bhattacharyya Distance[2]. It found such a "
+                         "change in " metric_name " on " date " so it asked its buddy medusa[3] to send this email to the "
+                         "dev-telemetry-alerts mailing list and to all email addresses listed in the alert_emails field of "
+                         metric_name "'s definition."
+                         "\n\n"
+                         "You can do this!"
+                         "\n\n"
+                         "Your Friendly, Neighbourhood Firefox Telemetry Team"
+                         "\n"
+                         "[1]: https://github.com/mozilla/cerberus"
+                         "\n"
+                         "[2]: https://en.wikipedia.org/wiki/Bhattacharyya_distance"
+                         "\n"
+                         "[3]: https://github.com/mozilla/medusa"
+                         "\n")
                     (concat subscribers foreign_subscribers ["dev-telemetry-alerts@lists.mozilla.org"])))
       (catch Throwable e ; could not find revisions for the given build date
         (log/info e "Retrieving changeset failed")
